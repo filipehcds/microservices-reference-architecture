15f0fb26dc9c3beaa710317baf57de045650012a
Filipe Henrique <filipehcds.sp@gmail.com>
2018-07-21 15:49:03 -0300

    Initial commit to Test Service, a demo Micro Service to test all the Spring Boot features.

---
+++ test-service/src/main/java/br/com/microservices/reference/architecture/testservice/TestServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.microservices.reference.architecture.testservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestServiceApplication.class, args);
+	}
+}
