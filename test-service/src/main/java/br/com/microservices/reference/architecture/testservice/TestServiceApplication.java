4bcec46299a84ce8e1a50cb842bc7031ed373175
Filipe Henrique <filipehcds.sp@gmail.com>
2018-07-22 02:33:03 -0300

    Added Cloud Service Discovery with Eureka Server/Client

--- test-service/src/main/java/br/com/microservices/reference/architecture/testservice/TestServiceApplication.java
+++ test-service/src/main/java/br/com/microservices/reference/architecture/testservice/TestServiceApplication.java
@@ -2,9 +2,11 @@ package br.com.microservices.reference.architecture.testservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = { "br.com.microservices.reference.architecture.testservice" })
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TestServiceApplication {
 
