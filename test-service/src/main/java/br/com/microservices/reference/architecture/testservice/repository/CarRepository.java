2f8444b06c63e6f64b8a3f673bb86c1dbeb00fdb
Filipe Henrique <filipehcds.sp@gmail.com>
2019-05-01 22:10:09 -0300

    Remoção

--- test-service/src/main/java/br/com/microservices/reference/architecture/testservice/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.microservices.reference.architecture.testservice.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.microservices.reference.architecture.testservice.domain.Car;
-
-@Repository
-public interface CarRepository extends ReactiveMongoRepository<Car, String> { }
